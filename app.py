473a298656e210c16c3a829a3bc03f525f76b300
travisesselink <travis.esselink@gmail.com>
2022-12-08 16:30:34 +1100

    changed db url to heroku postgres

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
+
 if DATABASE_URL.startswith('postgres://'):
     DATABASE_URL = DATABASE_URL.replace('postgres://', 'postgresql://', 1)
 
